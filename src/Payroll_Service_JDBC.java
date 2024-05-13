2f060e35931dd05f25735e907d6a786ae98ee86f
Tushar <tushar.jadhav.work@gmail.com>
2024-05-13 16:18:16 +0530

    Ability for Employee Payroll Service to retrieve the Employee Payroll from the Database

--- src/Payroll_Service_JDBC.java
+++ src/Payroll_Service_JDBC.java
@@ -1,6 +1,5 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.List;
 
 public class Payroll_Service_JDBC {
 
@@ -14,6 +13,28 @@ public class Payroll_Service_JDBC {
         try {
             Connection con = DriverManager.getConnection(url, username, password);
             System.out.println("Successfully connected....");
+            String query = "SELECT * FROM employee_payroll";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()){
+                System.out.println("Employee ID : "+rs.getInt(1));
+                System.out.println("Employee Name : "+rs.getString(2));
+                System.out.println("Gender : "+rs.getString(3));
+                System.out.println("Salary : "+rs.getInt(4));
+                System.out.println("Start Date : "+rs.getDate(5));
+                System.out.println("Phone Number : "+rs.getInt(6));
+                System.out.println("Address : "+rs.getString(7));
+                System.out.println("Department : "+rs.getString(8));
+                System.out.println("Basic pay : "+rs.getDouble(9));
+                System.out.println("Deduction : "+rs.getDouble(10));
+                System.out.println("Taxable pay : "+rs.getDouble(11));
+                System.out.println("Income tax : "+rs.getDouble(12));
+                System.out.println("Net pay : "+rs.getDouble(13));
+                System.out.println();
+            }
+            System.out.println("Read data Successfully");
+            con.close();
         }
         catch (Exception e){
             System.out.println(e);
