61b5827892e53742a130d6876b799262555f9636
Tushar <tushar.jadhav.work@gmail.com>
2024-05-13 15:29:30 +0530

    Ability to create a payroll service database and have java program connect to database

--- src/Payroll_Service_JDBC.java
+++ src/Payroll_Service_JDBC.java
@@ -1,10 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Payroll_Service_JDBC {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
-        System.out.println("Welcome to payroll service database ");
+        String url = "jdbc:mysql://localhost:3306/payroll_service";
+        String username = "root";
+        String password = "1999";
+
+        try {
+            Connection con = DriverManager.getConnection(url, username, password);
+            System.out.println("Successfully connected....");
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
 
 
     }
